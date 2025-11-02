c5957e63eb69c22d8a1c112591952b3747ef8ae0
Kiruba Karan <kirubakaranatwork@gmail.com>
2025-11-02 14:36:09 +0530

    initial_commit

---
+++ src/test/java/com/example/account_management_system/AccountManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.account_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
